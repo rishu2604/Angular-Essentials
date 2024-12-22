28e9f63f59399e910bd59de23e8740d8c2178cee
Rishu Anand <rishu.anand@knowmax.ai>
2024-12-22 17:59:00 +0530

    feat: added ngContent and pipe output transformer

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,13 +10,14 @@ import {
 } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
